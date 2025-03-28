47048959338686477ab575c07e4638cf8596cf6a
AyeNyeinSan <aye2904@gmail.com>
2025-03-28 14:23:41 +0700

    Added modified myWord module for tokenization

--- testscript.py
+++ testscript.py
@@ -1,8 +1,12 @@
-from external.myWord.myword import tokenize_text
+import sys
+import os
+sys.path.append(os.path.abspath("external/myWord"))
 
+
+from myword import tokenize_text
 text = "ကျွန်တော်မြန်မာစာကို စမ်းသပ်နေပါတယ်။"
 tokens = tokenize_text(text)
 print(tokens)
 
-with open("output.txt", "w") as f:
+with open("output.txt", "w", encoding="utf-8") as f:
     f.write(tokens)
